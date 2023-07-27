a09794ca3b4e0e36111bcca10765506f81bd2fc7
Lucas Cerqueira <lucas44r@gmail.com>
2023-07-27 15:54:25 +0100

    Guess the word using words from API

--- app/page.js
+++ app/page.js
@@ -1,45 +1,129 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import styles from "./page.module.css";
+import React, { useState, useEffect } from "react";
+const axios = require("axios").default;
+
+// create a npm package with all "most commom english word" from my repo in GH
 
 export default function Home() {
+  const [hasFoundWord, setHasFoundWord] = useState(false);
+  const [guesses, setGuesses] = useState([]);
+  const [userMessage, setUserMessage] = useState("");
+  const [randomWord, setRandomWord] = useState("");
+  const [dictionaryEntry, setDictionaryEntry] = useState({});
+
+  const [definition, setDefinition] = useState("");
+
+  const clearFields = () => {
+    setHasFoundWord(false);
+    setGuesses([]);
+    setUserMessage("");
+    document.getElementById("userInput").value = "";
+  };
+
+  const updateWord = () => {
+    clearFields();
+    const randomWordsApiUrl = "https://random-word-api.herokuapp.com/word";
+    const dictionaryApiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+    axios
+      .get(randomWordsApiUrl)
+      .then((res) => {
+        const newWord = res.data[0];
+        axios
+          .get(dictionaryApiUrl + newWord)
+          .then((resDict) => {
+            const dictionaryDefinition = resDict.data[0];
+            setRandomWord(newWord);
+            setDictionaryEntry(dictionaryDefinition);
+            extractDefinition(dictionaryDefinition);
+          })
+          .catch(function (error) {
+            updateWord();
+            console.error(error);
+          });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const extractDefinition = (dict) => {
+    const partOfSpeech = dict.meanings[0].partOfSpeech;
+    const definition = dict.meanings[0].definitions[0].definition;
+
+    setDefinition(`${partOfSpeech}: ${definition}`);
+  };
+
+  useEffect(updateWord, []);
+
+  function handleKeyPress(e) {
+    if (e.keyCode === 13) {
+      if (hasFoundWord) return;
+      const word = e.target.value;
+      if (word === randomWord) {
+        setHasFoundWord(true);
+        setUserMessage("");
+      } else {
+        if (!guesses.includes(word)) {
+          setGuesses([word, ...guesses]);
+          setUserMessage("");
+        } else {
+          setUserMessage(`${word} was already guessed! Try again.`);
+        }
+        e.target.value = "";
+      }
+    }
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      {hasFoundWord && (
+        <div className={styles.noOverflow}>
+          <div className={styles.firework}></div>
+          <div className={styles.firework}></div>
+          <div className={styles.firework}></div>
         </div>
+      )}
+      <h1 className={styles.title}>Guess the word</h1>
+
+      <div>
+        <p className={styles.definitionTitle}>Definition</p>
+        <p className={styles.definition}>{definition}</p>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className={styles.userInputDiv}>
+        <input
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          id="userInput"
+          className={styles.userInput}
+          type="text"
+          onKeyDown={(e) => handleKeyPress(e)}
         />
+        <p>{userMessage}</p>
       </div>
 
-      <div className={styles.grid}>
+      {hasFoundWord && (
+        <div className={styles.getNewWordDiv}>
+          <p className={styles.victory}>Correct! The word is {randomWord}!</p>
+          <p>
+            <button className={styles.getNewWordButton} onClick={updateWord}>
+              New word
+            </button>
+          </p>
+        </div>
+      )}
+
+      <div className={styles.guess}>
+        <ul>
+          {guesses.length > 0 && guesses.map((g) => <li key={g}>{g}</li>)}
+        </ul>
+      </div>
+
+      {/* <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
@@ -89,7 +173,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
-  )
+  );
 }
