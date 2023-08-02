429b2298cba8824b530bd570c05f19d8b8e1207c
Lucas Saar Cerqueira <lucas44r@gmail.com>
2023-08-02 14:17:10 +0100

    Add word distance calculation

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import styles from "./page.module.css";
+import isWordClose from "./word-distance";
 import React, { useState, useEffect } from "react";
 const axios = require("axios").default;
 
@@ -60,13 +61,14 @@ export default function Home() {
   function handleKeyPress(e) {
     if (e.keyCode === 13) {
       if (hasFoundWord) return;
-      const word = e.target.value;
+      let word = e.target.value;
       if (word === randomWord) {
         setHasFoundWord(true);
         setUserMessage("");
       } else {
-        if (!guesses.includes(word)) {
-          setGuesses([word, ...guesses]);
+        if (!guesses.map(x => x.word).includes(word)) {
+          const isClose = isWordClose(word, randomWord);
+          setGuesses([{ "word": word, "isClose": isClose }, ...guesses]);
           setUserMessage("");
         } else {
           setUserMessage(`${word} was already guessed! Try again.`);
@@ -119,61 +121,10 @@ export default function Home() {
 
       <div className={styles.guess}>
         <ul>
-          {guesses.length > 0 && guesses.map((g) => <li key={g}>{g}</li>)}
+          {guesses.length > 0 && guesses.map((g) => <li key={g.word}>{g.word} {g.isClose && <span> is close!</span>}</li>)}
         </ul>
       </div>
 
-      {/* <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
     </main>
   );
 }
---
+++ app/word-distance.js
@@ -0,0 +1,48 @@
+const levenshteinDistance = (s, t) => {
+    if (!s.length) return t.length;
+    if (!t.length) return s.length;
+    const arr = [];
+    for (let i = 0; i <= t.length; i++) {
+      arr[i] = [i];
+      for (let j = 1; j <= s.length; j++) {
+        arr[i][j] =
+          i === 0
+            ? j
+            : Math.min(
+                arr[i - 1][j] + 1,
+                arr[i][j - 1] + 1,
+                arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1)
+              );
+      }
+    }
+    return arr[t.length][s.length];
+  };
+
+  // threshold for being close is fixed for word with length 1 to 10:
+  const threshold = {
+    2: 1,
+    3: 1,
+    4: 1,
+    5: 2,
+    6: 2,
+    7: 2,
+    8: 3,
+    9: 3,
+    10:3
+  };
+
+  const isWordClose = (guess, target) => {
+
+    const dist = levenshteinDistance(guess, target);
+    
+    if(guess.length <= 10){
+        return dist <= threshold[guess.length];
+    }
+
+    // calculates the % of the distance according to the guess word size
+    const percentDist = dist / guess ;
+
+    return percentDist <= 0.3;
+  }
+
+  export default isWordClose;
